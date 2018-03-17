c1174dc874c6696a31049f1c1138874c5ec9a2bd
Danil <d.dovgal.kiev@gmail.com>
2018-03-17 17:37:47 +0200

    Complete task Deodorant Evaporator.

---
+++ src/deodorantEvaporator/DeodorantEvaporatorMain.java
@@ -0,0 +1,7 @@
+package deodorantEvaporator;
+
+public class DeodorantEvaporatorMain {
+    public static void main(String[] args) {
+        System.out.println(DeodorantEvaporator.evaporator(10, 10, 10));
+    }
+}
