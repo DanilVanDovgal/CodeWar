9eb4a2140c6e7cd5fcf944f85fecd09e356fd108
Danil <d.dovgal.kiev@gmail.com>
2018-03-17 22:02:54 +0200

    Complete task Printer Error.

---
+++ src/printerError/PrinterErrorMain.java
@@ -0,0 +1,8 @@
+package printerError;
+
+public class PrinterErrorMain {
+    public static void main(String[] args) {
+        System.out.println(PrinterError.printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz\n" +
+                "kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"));
+    }
+}
